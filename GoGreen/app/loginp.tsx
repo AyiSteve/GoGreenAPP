805ed9d8c1a6f77e8c5e78f18dc68ee811593c16
Steven Gong <stevegwy@Stevendebijibendiannao.local>
2025-10-19 01:09:00 -0700

    c

--- GoGreen/app/loginp.tsx
+++ GoGreen/app/loginp.tsx
@@ -35,7 +35,7 @@ export default function UserScreen() {
       });
 
   try {
-        const res = await fetch("http://10.0.0.75:5002/user/signup", {
+        const res = await fetch("http://10.0.0.75:5002", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(userData.toJSON()),
